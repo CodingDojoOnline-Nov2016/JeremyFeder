80931adadcf3468bf8d07b5423d29a495e8ef7e0
Jeremy Feder <J.Psycle@gmail.com>
2016-11-14 00:32:04 -0500

    Ninja To Cat/Archive.zip

---
+++ Ninja To Cat/js/NinjaToCat.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  $('img').click(function() {
+    console.log('data-alt-src value is', $(this).attr('data-alt-src'));
+    $(this).attr('src', $(this).attr('data-alt-src'));
+  });
+
+})
