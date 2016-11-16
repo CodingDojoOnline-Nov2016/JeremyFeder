e491f9d2f7123758c2a038b1bd5946ad93157bdb
Jeremy Feder <J.Psycle@gmail.com>
2016-11-16 12:19:17 -0500

    couldn't really get jQuery Add User to work at this time :(

--- Disappearing Ninja/js/Dis Ninja.js
+++ Disappearing Ninja/js/Dis Ninja.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
 
   $("img").click(function() {
-    $(this).hide("slow", function() {
-    });
+    $(this).hide("slow")
   });
 
   $("button").click(function() {
-    $("img").show("slow", function() {
-    });
+    $("img").show("slow")
   });
 
 })
--- Ninja To Cat/js/NinjaToCat.js
+++ Ninja To Cat/js/NinjaToCat.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 
   $('img').click(function() {
-    console.log('data-alt-src value is', $(this).attr('data-alt-src'));
+    var temp = $(this).attr('src');
     $(this).attr('src', $(this).attr('data-alt-src'));
+    $(this).attr('data-alt-src', temp)
   });
 
 })
