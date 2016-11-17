617de605fd157394367409d3433bd8d3229730a2
Jeremy Feder <J.Psycle@gmail.com>
2016-11-17 15:41:26 -0500

    adding sortable ninja

---
+++ Sortable Ninja/js/SortableNinja.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  // $('img').click(function() {
+  //   var temp = $(this).attr('src');
+  //   $(this).attr('src', $(this).attr('data-alt-src'));
+  //   $(this).attr('data-alt-src', temp)
+  // });
+
+  $( function() {
+      $( "#sortable" ).sortable({
+        revert: true
+      });
+      $( "#draggable" ).draggable({
+        connectToSortable: "#sortable",
+        helper: "clone",
+        revert: "invalid"
+      });
+      $( "ul, li" ).disableSelection();
+    } );
+
+})
