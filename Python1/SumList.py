5be3b295ea6365b688f8e394fa2c040d127aa867
Jeremy Feder <J.Psycle@gmail.com>
2016-11-29 14:47:46 -0500

    adding SumList & AvgList Python

---
+++ Python1/SumList.py
@@ -0,0 +1,13 @@
+
+#  In-built Math Solution
+a = [1, 2, 5, 10, 255, 3]
+sumofnum = sum(list(a))
+print sumofnum
+
+
+#  Loop Solution
+a = [1,2,5,10,255,3]
+sum = 0
+for number in a:
+    sum += number
+print sum
