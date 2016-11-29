a582eeb9387242eeff83e25da27a4fa214d00123
Jeremy Feder <J.Psycle@gmail.com>
2016-11-29 12:57:05 -0500

    added additional solutions to multiples

--- Python1/Multiples.py
+++ Python1/Multiples.py
@@ -7,3 +7,11 @@ for num in range (1, 1000+1):
 for multiple in range (5, 1000000+1):
     if multiple % 5 == 0:
         print multiple
+
+# Part 1b
+for num in range (1, 1000+1, 2):
+    print num
+
+# Part 2b
+for multiple in range (5, 100+1, 5):
+    print multiple
