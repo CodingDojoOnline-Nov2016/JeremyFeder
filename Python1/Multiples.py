76cea654996a3f22276758bb67bfa02067204bb6
Jeremy Feder <J.Psycle@gmail.com>
2016-11-29 12:48:47 -0500

    added Python Multiples

---
+++ Python1/Multiples.py
@@ -0,0 +1,9 @@
+# Part 1
+for num in range (1, 1000+1):
+    if num % 2 != 0:
+        print num
+
+# Part 2
+for multiple in range (5, 1000000+1):
+    if multiple % 5 == 0:
+        print multiple
