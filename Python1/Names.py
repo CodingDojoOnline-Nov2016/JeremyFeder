84b1b46a0477e6e4db8949c4984ce3d22e5b194a
Jeremy Feder <J.Psycle@gmail.com>
2016-12-01 16:15:01 -0500

    added Names

---
+++ Python1/Names.py
@@ -0,0 +1,11 @@
+students = {
+    'names': [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+}
+for key, data in students.items():
+    for value in data:
+        print value['first_name'], value['last_name']
