9f039f5eb6743a203c88e9c2c7493139abe75235
Jeremy Feder <J.Psycle@gmail.com>
2016-11-30 21:03:40 -0500

    a working robust ScoresGrades

--- Python1/ScoresGrades.py
+++ Python1/ScoresGrades.py
@@ -1,20 +1,26 @@
 print "Scores and Grades"
-def score_grade(score):
-    for count in range (0, 10, 1):
-        if score >= 60 and score <= 69:
+def score_grade():
+    count = 1
+    while count <= 10:
+        print "Please enter your score."
+        try:
+            score = int(raw_input())
+        except:
+            print "NOPE!"
+            score = 0
+        if score < 60 or score > 100:
+            print "Please enter a score between 60 and 100"
+        elif score < 70:
             print "Score:", score,";","Your grade is D"
             count += 1
-        elif score >= 70 and score <= 79:
+        elif score < 80:
             print "Score:", score,";","Your grade is C"
             count += 1
-        elif score >= 80 and score <= 89:
+        elif score < 90:
             print "Score:", score,";","Your grade is B"
             count += 1
-        elif score >= 90 and score <= 100:
+        else:
             print "Score:", score,";","Your grade is A"
             count += 1
-        else:
-            print "Please enter a score between 60 and 100"
-score_grade(77)
+score_grade()
 print "End of program. Bye!"
-# Could not figure out how to make a seperate prompt per iteration. Best semi-working version I could come up with :-?
