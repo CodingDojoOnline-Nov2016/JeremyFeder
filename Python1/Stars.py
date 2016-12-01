a950c567233eadab73c82ff8e0bdfaf9a3fb34e0
Jeremy Feder <J.Psycle@gmail.com>
2016-12-01 13:55:35 -0500

    adding Draw Stars

---
+++ Python1/Stars.py
@@ -0,0 +1,17 @@
+
+x = [4, 6, 1, 3, 5, 7, 25]
+def draw_stars(list):
+    for stars in list:
+        print stars * "*"
+draw_stars(x)
+
+
+x2 = [4, "Tom", 1, "Michael", 5, 7, "Jimmy Smith"]
+def draw_stars(list):
+    for stars in list:
+        if type(stars) == int:
+            print stars * "*"
+        else:
+            first_init = stars[0].lower()
+            print first_init * len(stars)
+draw_stars(x2)
