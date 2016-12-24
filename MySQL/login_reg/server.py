b8a44629b45b33b276b4fac2b721a12767e5d2e6
Jeremy Feder <J.Psycle@gmail.com>
2016-12-23 20:09:56 -0500

    final fully-functional Login and Registration project

--- MySQL/login_reg/server.py
+++ MySQL/login_reg/server.py
@@ -16,13 +16,29 @@ app.secret_key = 'supertopsecret'
 def index():
     return render_template('index.html')
 
-@app.route('/create', methods=['POST'])
+@app.route('/users/<id>/profile')
+def users(id):
+	query = "SELECT first_name, last_name FROM users WHERE id = :id"
+	data = { 'id': id }
+	user = mysql.query_db(query, data)
+	return render_template('success.html', user=user)
+
+@app.route('/users', methods=['POST'])
 def create():
     errors = []
+
+    #Check to see if user email already exists in the system
+    email = request.form['email']
+    email_query = "SELECT id FROM users WHERE email= :email"
+    email_data = { 'email': email }
+    user_check = mysql.query_db(email_query, email_data)
+    if user_check:
+        errors.append('A user already exists with that email in the system.  Please Login or try again.')
 
     if len(request.form['first_name']) < 2:
         errors.append('You must enter a First Name of at least 2 characters!')
     elif not name_regex.match(request.form['first_name']):
+    # elif not first_name.isalpha()   <-another way to check
         errors.append('Your first name may not contain any special characters or numbers!')
 
     if len(request.form['last_name']) < 2:
@@ -59,19 +75,37 @@ def create():
         }
 
         mysql.query_db(query, data)
-        return redirect('/success')
 
+        #pull user_id for routing
+        user_query = "SELECT id FROM users WHERE email = :email LIMIT 1"
+        user_data = { 'email': email }
+        user = mysql.query_db(user_query, user_data)
+        flash("successful registration")
+        session['user_id'] = user[0]['id']
+        return redirect('/users/'+str(session['user_id'])+'/profile')
 
-@app.route('/success')
-def success():
-    # query = "SELECT (first_name, last_name, email) FROM users"
-    # data = {
-    #     'first_name':request.form['first_name'],
-    #     'last_name':request.form['last_name'],
-    #     'email':request.form['email'],
-    # }
-    # emails = mysql.query_db(query, data)
-    return render_template('success.html')
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+    if email and password:
+        user_query = "SELECT id, email, password FROM users WHERE email = :email LIMIT 1"
+        query_data = { 'email': email }
+        try:
+            user = mysql.query_db(user_query, query_data)
 
+            if bcrypt.check_password_hash(user[0]['password'], password):
+                session['user_id'] = user[0]['id']
+                flash('successful login')
+                return redirect('/users/'+str(session['user_id'])+'/profile')
+            else:
+                flash('incorrect password')
+                return redirect('/')
+        except:
+            flash('no user exists with that email')
+            return redirect('/')
+    else:
+        flash('unsuccessful login')
+        return redirect('/')
 
 app.run(debug=True)
