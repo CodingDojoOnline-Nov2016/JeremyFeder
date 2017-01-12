9fbabe9c0720c8c58017686c568b620caaaab526
Jeremy Feder <J.Psycle@gmail.com>
2017-01-12 15:05:16 -0500

    betterized Time Display now with correct timezone

--- Django/time_display_assignment/apps/timedisplay/views.py
+++ Django/time_display_assignment/apps/timedisplay/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, HttpResponse
-import datetime
+from django.utils import timezone
+# import datetime
+
 
 # Create your views here.
 def index(request):
-    i = datetime.datetime.now()
-    currentDateTime = ("%s/%s/%s" % (i.day,i.month,i.year)) + (" %s:%s:%s" % (i.hour,i.month,i.second))
-
+    # i = datetime.datetime.now()
+    # currentDateTime = ("%s/%s/%s" % (i.month,i.day,i.year)) + (" %s:%s:%s" % (i.hour-5,i.minute,i.second))
+    #
+    # context = {
+    # "currentDateTime":currentDateTime
+    # }
+    now = timezone.now()
     context = {
-    "currentDateTime":currentDateTime
+        'now': now
     }
     return render(request, 'timedisplay/index.html', context)
