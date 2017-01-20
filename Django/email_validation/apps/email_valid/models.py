694a1466d0fa3df58ec7ddb06ce7e288fa4d2ce9
Jeremy Feder <J.Psycle@gmail.com>
2017-01-20 16:06:38 -0500

    added bonus nifty feature to Email Validation to check for existing Email prior to adding to db

--- Django/email_validation/apps/email_valid/models.py
+++ Django/email_validation/apps/email_valid/models.py
@@ -14,21 +14,22 @@ class EmailManager(models.Manager):
         elif not EMAIL_REGEX.match(email):
             errors.append("This Email Address appears to be invalid")
         ## Make sure email is unique
-        # elif email == checkEmail:
-        #     errors.append("An Email Address by that name already exists!  Try a new one.")
+        elif Email.objects.filter(email=email):
+            errors.append("An Email Address by that name already exists!  Try a new one.")
+
         if errors:
             return (False, errors)
         else:
-            e = Email.emailManage.create(email=email)
+            e = Email.objects.create(email=email)
             e.save()
             return (True, e)
 
     def destroy(self, id):
-        e = Email.emailManage.get(id=id)
+        e = Email.objects.get(id=id)
         e.delete()
 
 class Email(models.Model):
     email = models.CharField(max_length = 80)
     created_at = models.DateField(auto_now_add = True)
     updated_at = models.DateField(auto_now = True)
-    emailManage = EmailManager()
+    objects = EmailManager()
--- Django/email_validation/apps/email_valid/views.py
+++ Django/email_validation/apps/email_valid/views.py
@@ -7,7 +7,7 @@ def index(request):
 
 def create(request):
     if request.method == "POST":
-        result = Email.emailManage.register(request.POST['email'])
+        result = Email.objects.register(request.POST['email'])
         if result[0]:
             #Displays entered email as the validated email in Success
             request.session['email'] = result[1].email
@@ -22,9 +22,9 @@ def create(request):
         return redirect('/')
 
 def success(request):
-    emails = Email.emailManage.all()
+    emails = Email.objects.all()
     return render(request, 'email_valid/success.html', {'emails':emails, 'your_email': request.session.get('email')})
 
 def destroy(request, id):
-    result = Email.emailManage.destroy(id)
+    result = Email.objects.destroy(id)
     return redirect('/success')
